b305a0f1a012469721068975b65cd2851d23755b
Caval <scott.bui97@gmail.com>
2015-11-25 12:29:48 +0700

    random dish

--- LugagiWM/bin/Debug/AppX/script/index/index.js
+++ LugagiWM/bin/Debug/AppX/script/index/index.js
@@ -1,14 +1,25 @@
 ﻿$('body').on("click", "#goToIngredient", function () {
     WinJS.Navigation.navigate("/pages/recommendation/ingredientBasedSuggestion.html"); // navigate to ingredientBasedSuggestion page
-
 });
 
 $('body').on("click", "#goToWeek", function () {
     WinJS.Navigation.navigate("/pages/recommendation/weekMenuSuggestion.html"); // navigate to weekMenuSuggestion page
 });
 
-WinJS.UI.Pages.define("/pages/index.html", {
+// do when the page is ready
+WinJS.UI.Pages.define("/pages/index/index.html", {
     ready: function (element, options) {
-        var viewWidth = window.innerWidth;
+        // get random dish
+        $.ajax({
+            url: "http://lugagi.com/script/food/generateRandomFood.php",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                $("#randName").text(data.Foods[0].MonAnName);
+                var fullImgURL = "http://lugagi.com/script/timthumb.php?src=/foodimages/" + data.Foods[0].ImageURL + "&w=500&h=200";
+                console.log(fullImgURL);
+                $("#randImg").attr("src", fullImgURL)
+            }
+        });
     }
 });
