b1224df9ea16aef36aac4154ae4824e0afc26823
Caval <scott.bui97@gmail.com>
2015-11-29 15:25:06 +0700

    weekSuggestion
    Done

--- LugagiWM/bin/Debug/AppX/script/recommendation/weekMenuSuggestionContent.js
+++ LugagiWM/bin/Debug/AppX/script/recommendation/weekMenuSuggestionContent.js
@@ -1,17 +1,16 @@
 ﻿var dayMenuHTML;
 var mealSectionList = [".breakfastSection", ".lunchSection", ".dinnerSection"];
-var contentItemHTML = '<div class="contentItem col-xs-6 col-sm-4 col-md-3 col-lg-2">' +
-                           '<img class="contentImg img-responsive" src="" />' +
-                           '<h6 class="contentName"></h6>' +
-                           '<small>' +
-                               //'<span class="glyphicon glyphicon-eye-open contentView"></span>' +
-                               //'<span class="glyphicon glyphicon-heart contentLike"></span>' +
-                               '<span class="contentID" style="display:none"></span>' +
-                               '<span class="contentType" style="display:none">food</span>' +
-                           '</small>' +
+var contentItemHTML = '<div class="menuContentCol col-xs-6 col-sm-4 col-md-3 col-lg-2">' +
+                           '<div class="menuContentItem" data-food-ID="">' +
+                               '<img class="menuContentImg img-responsive" src="" />' +
+                               '<h6 class="menuContentName"></h6>' +
+                               //'<small>' +
+                                   //'<span class="glyphicon glyphicon-eye-open contentView"></span>' +
+                                   //'<span class="glyphicon glyphicon-heart contentLike"></span>' +
+                               //'</small>' +
+                            '</div>' +
+                            //'<button class="changeMenuItem specialButton" data-day="" data-meal="" data-dish="" changed="">Change Dish</button>' +
                        '</div>';
-
-
 
 // reload the contents
 function reloadContent() {
@@ -23,29 +22,37 @@ function reloadContent() {
 }
 
 // load a food item content
-function loadContent(mealSectionElem, source, index) {
+function loadMenuItemContent(mealSectionElem, source, index) {
     var fullImgURL = "http://lugagi.com/script/timthumb.php?src=/foodimages/" + source.ImageURL + "&w=300&h=200";
-    mealSectionElem.find(".contentImg").eq(index).attr("src", fullImgURL);
-    mealSectionElem.find(".contentName").eq(index).text(source.MonAnName);
-    mealSectionElem.find(".contentID").eq(index).text(source.MonAnID);
+    mealSectionElem.find(".menuContentImg").eq(index).attr("src", fullImgURL);
+    mealSectionElem.find(".menuContentName").eq(index).text(source.MonAnName);
+    mealSectionElem.find(".menuContentItem").eq(index).attr("data-food-ID", source.MonAnID);
+    mealSectionElem.find(".changeMenuItem").eq(index).attr("data-dish", index);
 }
 
 // load the week menu page
 function loadWeekMenuContent(data) {
     var i = 0;
+    // load each day
     for (i; i < 7; i++) {
         var daySource = data[i].Meal;
         var dayID = ".tab-pane:eq(" + i + ")";
         var j = 0;
+        console.log(i);
+        // load each meal
         for (j; j < 3; j++) {
             var mealSource = daySource[j];
             var mealSectionElem = $(dayID).find(mealSectionList[j]);
             var k = 0;
+            // load each dish
             for (k; k >= 0; k++) {
                 var dishSource = mealSource[k];
+                // break the loop if out of dishes
                 if (dishSource) {
                     mealSectionElem.append(contentItemHTML);
-                    loadContent(mealSectionElem, dishSource, k);
+                    mealSectionElem.find(".changeMenuItem").eq(k).attr("data-day", i);
+                    mealSectionElem.find(".changeMenuItem").eq(k).attr("data-meal", j);
+                    loadMenuItemContent(mealSectionElem, dishSource, k);
                 } else {
                     break;
                 }
@@ -54,12 +61,34 @@ function loadWeekMenuContent(data) {
     }
 }
 
+// navigate to foodDetails page of the dish
+function goToDish() {
+    var contentID = $(this).attr("data-food-ID");
+    WinJS.Navigation.navigate("/pages/food/foodDetails.html", contentID);
+}
+$('body').on("click", ".menuContentItem", goToDish);
+
+// new menu suggestion event
+$('body').on("click", "#newMenu", function () {
+    loadWeekMenu(nutritionChoices);
+})
+
+
+// for change menu dish feature (have not finished)
+
+//$('body').off("click", ".changeMenuItem", goToDish);
+
+//$('body').on("click", ".changeMenuItem", function () {
+//    var itemDay = $(this).attr("data-day");
+//    var itemMeal = $(this).attr("data-meal");
+//    var itemDish = $(this).attr("data-dish");
+//})
 
 
 WinJS.UI.Pages.define("/pages/recommendation/weekMenuSuggestionContent.html", {
     ready: function (element, options) {
         dayMenuHTML = $("#monday").html();
         reloadContent();
-        loadWeekMenuContent(options);
+        loadWeekMenuContent(options[0]);       
     }
 });
--- LugagiWM/bin/Debug/AppX/script/recommendation/weekMenuSuggestionFilter.js
+++ LugagiWM/bin/Debug/AppX/script/recommendation/weekMenuSuggestionFilter.js
@@ -1,46 +1,76 @@
-﻿var nutrionChoices = [true, false, false, false, false, false];
+﻿var nutritionChoices = [true];
 var checkBoxesElem;
 
+
+// load default menu (no filter)
+function loadDefaultMenu() {
+    $.ajax({
+        url: "http://lugagi.com/script/smartPhoneAPI/suggestion/weekMenuSuggestion/runMenuSuggestion.php",
+        data: "nothing",
+        dataType: 'json',
+        cache: false,
+        async: true,
+        success: function (data) {
+            WinJS.Navigation.navigate("/pages/recommendation/weekMenuSuggestionContent.html", [data, nutritionChoices]);
+        }
+    });
+}
+
+// load filtered menu
+function loadFilteredMenu(currentChoices) {
+    $.ajax({
+        type: "POST",
+        url: "http://lugagi.com/script/smartPhoneAPI/suggestion/weekMenuSuggestion/runMenuSuggestion.php",
+        data: { CheDoDinhDuong: currentChoices },
+        dataType: 'json',
+        cache: false,
+        async: true,
+        success: function (data) {
+            WinJS.Navigation.navigate("/pages/recommendation/weekMenuSuggestionContent.html", [data, nutritionChoices]);
+        }
+    });
+}
 
 // get the week menu from server and load the menu
 function loadWeekMenu(choices) {
     if (choices[0]) {
-        $.ajax({
-            url: "http://lugagi.com/script/smartPhoneAPI/suggestion/weekMenuSuggestion/runMenuSuggestion.php",
-            data: "nothing",
-            dataType: 'json',
-            cache: false,
-            async: true,
-            success: function (data) {
-                WinJS.Navigation.navigate("/pages/recommendation/weekMenuSuggestionContent.html", data);
-            }
-        });
+        loadDefaultMenu();
+    } else {
+        var currentChoices = [];
+        for (var n = 1; n < 6; n++) {
+            currentChoices.push(choices[n])
+        }
+        loadFilteredMenu(currentChoices);
     }
-
 }
 
+// check if the checkboxes are checked
 $('body').on("change", ".checkbox", function () {
     var checkedStatus = $(this).find("input").prop("checked");
     var checkBoxIndex = checkBoxesElem.index(this);
-    nutrionChoices[checkBoxIndex + 1] = checkedStatus;
 })
 
+// add the chosen choices by counting the checked boxes
 $('body').on("click", "#weekSuggestion", function () {
-    var noOfCheckBox = checkBoxesElem.length;    
+    var noOfCheckBox = checkBoxesElem.length;
+    nutritionChoices = [true];
     var i = 0;
     for (i; i < noOfCheckBox; i++) {
-        if (checkBoxesElem.eq(i).find("input").prop("checked")) {
-            nutrionChoices[0] = false;
+        var currentCheckBox = checkBoxesElem.eq(i).find("input");
+        if (currentCheckBox.prop("checked")) {
+            nutritionChoices[0] = false;
+            nutritionChoices.push(i + 1);            
         }
     }
-    loadWeekMenu(nutrionChoices);
-
+    loadWeekMenu(nutritionChoices);
 })
 
+// uncheck all boxes
 $('body').on("click", "#clearBoxes", function(){
     checkBoxesElem.find("input").prop("checked", false);
-    nutrionChoices[0] = true;
+    nutritionChoices[0] = true;
 })
+
 
 WinJS.UI.Pages.define("/pages/recommendation/weekMenuSuggestionFilter.html", {
     ready: function (element, options) {
