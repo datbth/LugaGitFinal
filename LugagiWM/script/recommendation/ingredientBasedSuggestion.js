8ce3867442abb3deaa2617f192dcbc6b614c327f
Hainguyen1210 <hainguyen.hainguyen1210@gmail.com>
2015-12-11 17:54:18 +0700

    fixed

--- LugagiWM/script/recommendation/ingredientBasedSuggestion.js
+++ LugagiWM/script/recommendation/ingredientBasedSuggestion.js
@@ -1,51 +1,88 @@
-﻿var currentName = "";
+﻿var currentInput = "";
 var initialIngredientHTML;
 
 function loadIngredientSuggestion() {
     $.ajax({
         type: "GET",
         url: "http://lugagi.com/script/ingredientRecommendation/generateFoodSuggestion.php",
-        data: { ingredientname: [currentName] },
+        data: { ingredientname: [currentInput] },
         dataType: 'json',
         cache: false,
         async: true,
         success: function (receivedData) {
-            var source = receivedData.Foods
+            var source = receivedData.Foods;
             var noOfFood = source.length;
-            for (var i = 0; i < noOfFood; i++) {
-                var currentSource = source[i]
-                var newFood = $("#sampleIngredientContentItem").clone();
-                newFood.find(".ingredientContentName").text(currentSource.MonAnName);
-                var fullImageURL = "http://lugagi.com/script/timthumb.php?src=/foodimages/" + currentSource.ImageURL + "&w=300&h=200";
-                newFood.find(".ingredientContentImg").attr("src", fullImageURL);
-                newFood.attr("data-food-ID", currentSource.MonAnID);
-                newFood.css("display", "block");
-                $("#suggestionContent").append(newFood);
+            if (noOfFood < 1) {
                 $("progress").hide();
-            }        
+                $("#no-dish-mess").show();
+            } else {
+                for (var i = 0; i < noOfFood; i++) {
+                    var currentSource = source[i]
+                    var newFood = $("#sampleIngredientContentItem").clone();
+                    newFood.find(".ingredientContentName").text(currentSource.MonAnName);
+                    var fullImageURL = "http://lugagi.com/script/timthumb.php?src=/foodimages/" + currentSource.ImageURL + "&w=300&h=200";
+                    newFood.find(".ingredientContentImg").attr("src", fullImageURL);
+                    newFood.attr("data-food-ID", currentSource.MonAnID);
+                    newFood.css("display", "block");
+                    $("#suggestionContent").append(newFood);
+                    $("progress").hide();
+                }
+            }
         }
     });
-}
+};
 
-$('body').on("click", "#submitUserInput", function () {
-    currentName = "";
+function generateSuggestion() {
+    currentInput = "";
     $("#suggestionContent").html(initialIngredientHTML);
-    $("progress").show();
+
     var numberOfInput = $(".ingredientInput").length;
     for (var i = 0; i < numberOfInput; i++) {
         var inputName = $(".ingredientInput:eq(" + i + ")").find("input").val();
-        currentName += inputName + " ";
+        currentInput += inputName + " ";
     };
-    loadIngredientSuggestion();
-    
-})
+    var testCurrentInput = currentInput.replace(" ", "")
+    if (testCurrentInput != "") {
+        $("#no-input-mess").hide();
+        $("#no-dish-mess").hide();
+        $("progress").show();
+        loadIngredientSuggestion();
+    } else {
+        $("#no-input-mess").show();
+    }
+};
 
-$('body').on("click", "#addInput", function () {
+function addInputRow() {
     var newIngredientInput = $("#sampleIngredient").clone();
     newIngredientInput.attr("id", "");
     newIngredientInput.find("input").val("");
     newIngredientInput.find(".removeInput").show();
     $("#userInput").append(newIngredientInput);
+}
+
+$('body').on("click", "#submitUserInput", function () {
+    generateSuggestion();
+})
+
+$('body').on("keyup", ".ingredientInput input", function (e) {
+    if (e.keyCode == 13) {
+        generateSuggestion();
+    } else {
+        if (e.keyCode == 27) {
+            $(this).val("");            
+        }
+        else if (e.keyCode == 190) {
+            $(this).val($(this).val().slice(0, -1));
+            addInputRow();
+            $(".ingredientInput").find("input").last().focus();
+        }
+        $("#no-input-mess").hide();
+        $("#no-dish-mess").hide();
+    }
+})
+
+$('body').on("click", "#addInput", function () {
+    addInputRow();
 })
 
 $('body').on("click", ".removeInput", function () {
