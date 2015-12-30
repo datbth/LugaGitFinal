e0affe6cd603845788fb50bc1c378a7548010c02
Caval <scott.bui97@gmail.com>
2015-12-30 18:22:26 +0700

    done

--- LugagiWM/js/common.js
+++ LugagiWM/js/common.js
@@ -129,12 +129,12 @@ function navigate(evObject) {
     WinJS.Utilities.empty(host);
     // load new content
     evObject.detail.setPromise(
-        WinJS.UI.Pages.render(url, host, evObject.detail.state).then(function () {
+        WinJS.UI.Pages.render(url, host, state).then(function () {
             WinJS.Application.sessionState.lastUrl = url;
             WinJS.Application.sessionState.lastState = state;
             WinJS.UI.Animation.enterPage(host);
             WinJS.UI.processAll();
-        }));   
+        }));
 }
 
 //Get parameter from the string separated by "&"
@@ -462,7 +462,6 @@ var weekMenuScript = {
             async: true,
             success: function (data) {
                 WinJS.Navigation.navigate("/pages/recommendation/weekMenuSuggestionContent.html", [data, nutritionChoices]);
-                WinJS.Navigation.addEventListener("navigated", navigate);
             }
         });
     },
